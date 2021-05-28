dddb0ec6f09436774e066a0786ebc7b77c26aba6
DamianoD88 <dispe88@live.it>
2021-05-28 14:50:40 +0200

    add esecuzione

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Gioco dei dadi Generare un numero random da 1 a 6,
+// sia per il giocatore sia per il computer. 
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var giocatore = Math.floor(Math.random() * 7);
+
+var computer = Math.floor(Math.random() * 7);
+
+console.log('giocatore');
+
+console.log('computer');
+
+if () {
+    
+}
+
